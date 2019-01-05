ae22a60131d56c7791c9f4c39a1570ebcc4266a6
Alexandr Savinov <savinov@conceptoriented.org>
2019-01-05 17:33:07 +0100

    refactor: move model training into a separate function

--- lambdo/Column.py
+++ lambdo/Column.py
@@ -65,7 +65,7 @@ class Column:
         log.info("  ---> Start evaluating column '{0}'".format(self.id))
 
         #
-        # Stage 1: Ensure that "data" field is ready for applying column operations
+        # Stage 1: Ensure that the data field (with table data) is ready for applying column operations
         #
         table = self.table.data  # Table the columns will be added to
 
@@ -76,6 +76,7 @@ class Column:
         concrete_definitions = self.get_definitions()
         num_extensions = len(concrete_definitions)
 
+        # Essentially, we evaluate several columns independently
         for i, definition in enumerate(concrete_definitions):
 
             #
@@ -97,6 +98,7 @@ class Column:
             # Stage 4. Prepare input data argument to pass to the function (as the first argument)
             #
             data = table
+
             inputs = definition.get('inputs', [])
             inputs = get_columns(inputs, data)
             if inputs is None:
@@ -108,7 +110,7 @@ class Column:
                 log.warning("Not all columns available. Skip column definition.".format())
                 break
 
-            # Select only specified columns
+            # Select only the specified input columns
             data = data[inputs]
 
             data_type = definition.get('data_type')
@@ -129,77 +131,11 @@ class Column:
             train = definition.get('train')
             if not model and train:
 
-                # 1. Resolve train function
-                train_func_name = train.get('function')
-                train_func = resolve_full_name(train_func_name)
-                if not train_func:
-                    log.warning("Cannot resolve user-defined training function '{0}'. Skip training.".format(train_func_name))
+                model = self.train_model(definition, inputs)
+                if model is None:
                     break
 
-                # 2. Filter rows for train data
-                train_table = table
-                train_row_filter = train.get("row_filter")
-                if train_row_filter:
-                    train_table = apply_row_filter(table, train_row_filter)
-
-                # 3. Select columns to use for training
-                train_data = train_table
-                train_inputs = train.get('inputs')
-                if train_inputs is None:
-                    train_inputs = inputs  # Inherit from the 'apply' section
-                train_inputs = get_columns(train_inputs, train_data)
-                if train_inputs is None:
-                    log.warning("Error reading column list for training. Skip column training.")
-                    break
-
-                # Validation: check if all explicitly specified columns available
-                if not all_columns_exist(train_inputs, train_data):
-                    log.warning("Not all columns available for training. Skip column definition.".format())
-                    break
-
-                # Select only specified columns
-                train_data = train_data[train_inputs]
-
-                # 3. Determine labels
-                # - no labels at all (no argument is expected) - unsupervised learning
-                # - explicitly specified outputs
-                # - use output column specified in the transformation (but it has to be already available, e.g., loaded from source data, while the transformation will overwrite it)
-                labels = train.get('outputs')
-                if not labels:
-                    labels = definition.get('outputs')  # Same columns as used by the transformation
-
-                if labels:
-                    labels = get_columns(labels, table)
-                    if labels is None:
-                        log.warning("Error reading column list. Skip column definition.")
-                        break
-                    train_labels = train_table[labels]  # Select only specified columns
-                else:
-                    train_labels = None  # Do not pass any labels at all (unsupervised)
-
-                # 4. Retrieve hyper-model
-                train_model = train.get('model', {})
-
-                # Cast data argument
-                if data_type == 'ndarray':
-                    data_arg = train_data.values
-                    if train_labels is not None:
-                        labels_arg = train_labels.values
-                else:
-                    data_arg = train_data
-                    if train_labels is not None:
-                        labels_arg = train_labels
-
-                # 5. Call the function and generate a model
-                if train_labels is None:
-                    model = train_func(data_arg, **train_model)
-                else:
-                    if train_model is None:
-                        model = train_func(data_arg, labels_arg)
-                    else:
-                        model = train_func(data_arg, labels_arg, **train_model)
-
-                # 6. Each time a new model is generated, we store it in the model field of the definition
+                # Each time a new model is generated, we store it in the model field of the definition
                 if model and model_ref:
                     log.info("Store trained model in {0}.".format(model_ref))
                     set_value(model_ref, model)
@@ -243,6 +179,85 @@ class Column:
         #
 
         log.info("  <--- Finish evaluating column '{0}'".format(self.id))
+
+    def train_model(self, definition, inputs):
+
+        train = definition.get('train')
+        table = self.table.data
+
+        data_type = definition.get('data_type')
+
+        # 1. Resolve train function
+        train_func_name = train.get('function')
+        train_func = resolve_full_name(train_func_name)
+        if not train_func:
+            log.warning("Cannot resolve user-defined training function '{0}'. Skip training.".format(train_func_name))
+            return None
+
+        # 2. Filter rows for train data
+        train_table = table
+        train_row_filter = train.get("row_filter")
+        if train_row_filter:
+            train_table = apply_row_filter(table, train_row_filter)
+
+        # 3. Select columns to use for training
+        train_data = train_table
+        train_inputs = train.get('inputs')
+        if train_inputs is None:
+            train_inputs = inputs  # Inherit from the 'apply' section
+        train_inputs = get_columns(train_inputs, train_data)
+        if train_inputs is None:
+            log.warning("Error reading column list for training. Skip column training.")
+            return None
+
+        # Validation: check if all explicitly specified columns available
+        if not all_columns_exist(train_inputs, train_data):
+            log.warning("Not all columns available for training. Skip column definition.".format())
+            return None
+
+        # Select only specified columns
+        train_data = train_data[train_inputs]
+
+        # 3. Determine labels
+        # - no labels at all (no argument is expected) - unsupervised learning
+        # - explicitly specified outputs
+        # - use output column specified in the transformation (but it has to be already available, e.g., loaded from source data, while the transformation will overwrite it)
+        labels = train.get('outputs')
+        if not labels:
+            labels = definition.get('outputs')  # Same columns as used by the transformation
+
+        if labels:
+            labels = get_columns(labels, table)
+            if labels is None:
+                log.warning("Error reading column list. Skip column definition.")
+                return None
+            train_labels = train_table[labels]  # Select only specified columns
+        else:
+            train_labels = None  # Do not pass any labels at all (unsupervised)
+
+        # 4. Retrieve hyper-model
+        train_model = train.get('model', {})
+
+        # Cast data argument
+        if data_type == 'ndarray':
+            data_arg = train_data.values
+            if train_labels is not None:
+                labels_arg = train_labels.values
+        else:
+            data_arg = train_data
+            if train_labels is not None:
+                labels_arg = train_labels
+
+        # 5. Call the function and generate a model
+        if train_labels is None:
+            model = train_func(data_arg, **train_model)
+        else:
+            if train_model is None:
+                model = train_func(data_arg, labels_arg)
+            else:
+                model = train_func(data_arg, labels_arg, **train_model)
+
+        return model
 
 
 if __name__ == "__main__":
